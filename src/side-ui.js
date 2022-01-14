48c9e60bdc3f1003b6c3a57105a4015793b77660
Florian-Le-Menn <florian.lemenn29@gmail.com>
2022-01-14 17:56:33 +0100

    Help pop up

--- src/side-ui.js
+++ src/side-ui.js
@@ -1,3 +1,6 @@
+let clicked1;
+let popUpHovered = false;
+
 /**
  * This file contains the jQuery interactions that are not important for the core of the game
  */
@@ -42,5 +45,94 @@ $(function () {
 
     $("#population-amount-trade,#soldiers-amount-trade,#territories-amount-trade").on("keyup", function () {
         checkConfirmTradeButton();
+    });
+
+    $("#credits-link").click(function () {
+        $("#pop-up-container").attr("hidden", false);
+        $("#credits-pop-up").attr("hidden", false);
+        $('#credits-pop-up').hover(function () {
+            popUpHovered = true;
+        }, function () {
+            popUpHovered = false;
+        });
+    });
+
+
+    $("#help-link").click(function () {
+        $("#pop-up-container").attr("hidden", false);
+        $("#help-pop-up").attr("hidden", false);
+        $('#help-pop-up').hover(function () {
+            popUpHovered = true;
+        }, function () {
+            popUpHovered = false;
+        });
+    });
+
+    $("#pop-up-container").click(function () {
+        if (popUpHovered) return;
+        $("#pop-up-container>*").each(function () {
+            $(this).attr("hidden", true);
+        });
+        $("#pop-up-container").attr("hidden", true);
+    });
+
+    $("#btn-close-help").click(function () {
+        $("#pop-up-container").attr("hidden", true);
+        $("#help-pop-up").attr("hidden", true);
+    });
+
+    $("#credits-pop-up a").each(function () {
+        $(this).attr("target", "_blank");
+        $(this).attr("rel", "noopener noreferrer");
+    });
+
+    $("#btn-help1-2").click(function () {
+        $("#help-1").attr("hidden", true);
+        $("#help-2").attr("hidden", false);
+    });
+
+    $("#btn-help2-1").click(function () {
+        $("#help-2").attr("hidden", true);
+        $("#help-1").attr("hidden", false);
+    });
+
+    $("#btn-help2-3").click(function () {
+        $("#help-2").attr("hidden", true);
+        $("#help-3").attr("hidden", false);
+    });
+
+    $("#btn-help3-2").click(function () {
+        $("#help-3").attr("hidden", true);
+        $("#help-2").attr("hidden", false);
+    });
+
+    /**
+     * Listener to allow click and drag on map to fill selectors
+     */
+    $("path").each(function () {
+        $(this).on("mousedown", function () {
+            let style = $(this).attr('style');
+            let ifill = style.indexOf("fill:");
+            let color = style.substring(ifill + 5, ifill + 12);
+            let country = Country.findByColor(color);
+            clicked1 = country;
+
+        });
+        $(this).on("mouseup", function () {
+            let style = $(this).attr('style');
+            let ifill = style.indexOf("fill:");
+            let color = style.substring(ifill + 5, ifill + 12);
+            let country = Country.findByColor(color);
+            if (country == clicked1) {
+                clicked1 = null;
+                return;
+            }
+            if ($("#attacker-selector").hasClass("clickable")) {
+                setAttacker(clicked1);
+                setDefender(country);
+                setDonor(clicked1);
+                setReceiver(country);
+            }
+        });
     });
 });
